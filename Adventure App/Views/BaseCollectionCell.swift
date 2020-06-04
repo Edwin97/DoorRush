ff79629369e95fc45a5e7cd1e194cd19e8f867e4
edwin <c.edwin2011@gmail.com>
2020-06-04 23:59:09 +0800

    Custom parent cell collectionView

---
+++ Adventure App/Views/BaseCollectionCell.swift
@@ -0,0 +1,85 @@
+//
+//  BaseCollectionCell.swift
+//  Adventure App
+//
+//  Created by edwin on 03/06/2020.
+//  Copyright © 2020 edwin. All rights reserved.
+//
+
+import UIKit
+
+class BaseCollectionCell: UICollectionViewCell {
+    
+    let baseCell = "baseCell"
+    
+    var leftConstraint: NSLayoutConstraint?
+    var trailingConstraint: NSLayoutConstraint?
+    var topConstraint: NSLayoutConstraint?
+    var bottomConstraint: NSLayoutConstraint?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        registerClass()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints =  false
+        return collectionView
+    }()
+    
+    func setupViews() {
+        addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        leftConstraint = collectionView.leftAnchor.constraint(equalTo: leftAnchor)
+        leftConstraint?.isActive = true
+        
+        topConstraint = collectionView.topAnchor.constraint(equalTo: topAnchor)
+        topConstraint?.isActive = true
+        
+        trailingConstraint = collectionView.rightAnchor.constraint(equalTo: rightAnchor)
+        trailingConstraint?.isActive = true
+        
+        bottomConstraint = collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        bottomConstraint?.isActive = true
+    }
+    
+    func registerClass() {
+        collectionView.register(BaseCell.self, forCellWithReuseIdentifier: baseCell)
+    }
+}
+
+extension BaseCollectionCell: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: baseCell, for: indexPath) as! BaseCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.width / 4, height: collectionView.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+    }
+}
