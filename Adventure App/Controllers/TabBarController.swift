4a8f339f929a55abb04afa2ee199b11b24b10b76
edwin <c.edwin2011@gmail.com>
2020-06-04 23:57:18 +0800

    Set up TabBar Controller and remove navigation on tabBabr

--- Adventure App/Controllers/TabBarController.swift
+++ Adventure App/Controllers/TabBarController.swift
@@ -20,20 +20,34 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         super.viewWillAppear(animated)
         
         setupTabBar()
+        customTabBarFont()
+        
+        tabBar.barTintColor = .white
+        tabBar.tintColor = UIColor(named: "orange")
+        
+        
+    }
+    
+    func customTabBarFont() {
+        let appearance = UITabBarItem.appearance()
+        appearance.setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "TTNorms-Regular", size: 18)!], for: .normal)
+        appearance.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor(named: "grey")!], for: .normal)
+        appearance.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.systemRed], for: .selected)
     }
     
     func setupTabBar() {
-        let activityController = ActivityController()
-        activityController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "activityTabView"), tag: 0)
         
-        let exploreController = ExploreController()
-        exploreController.tabBarItem = UITabBarItem(title: "Explore", image: UIImage(named: "searchTabView"), tag: 0)
+        let activityController =  UINavigationController(rootViewController: ActivityController())
+        activityController.tabBarItem = UITabBarItem(title: "Delivery", image: UIImage(named: "activityTabView"), tag: 0)
         
-        let orderController = OrderController()
-        orderController.tabBarItem = UITabBarItem(title: "Order", image: UIImage(named: "orderTabView"), tag: 0)
+        let exploreController =  UINavigationController(rootViewController: ExploreController())
+        exploreController.tabBarItem = UITabBarItem(title: "Search", image: UIImage(named: "searchTabView"), tag: 0)
         
-        let settingController = SettingController()
-        settingController.tabBarItem = UITabBarItem(title: "Setting", image: UIImage(named: "settingTabView"), tag: 0)
+        let orderController = UINavigationController(rootViewController: OrderController())
+        orderController.tabBarItem = UITabBarItem(title: "Orders", image: UIImage(named: "orderTabView"), tag: 0)
+        
+        let settingController = UINavigationController(rootViewController: SettingController())
+        settingController.tabBarItem = UITabBarItem(title: "Account", image: UIImage(named: "settingTabView"), tag: 0)
         
         self.viewControllers = [activityController, exploreController, orderController, settingController]
     }    
